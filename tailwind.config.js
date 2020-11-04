3ef3ef5450329700158823b9e458d243945ac693
laithkawar <lkawar95@gmail.com>
2020-11-04 03:21:59 +0200

    General changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  purge: ["./public/**/*.html"],
   theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['Abdomaster']
-      }
+    // Some useful comment
+    fontFamily: {
+      'Tajawal': ['"Tajawal"', 'sans-serif'] // Ensure fonts with spaces have " " surrounding it.
     },
   },
-  variants: {},
-  plugins: [],
-};
+  variants: {
+    // Some useful comment
+  },
+  plugins: [
+    // Some useful comment
+  ]
+}
