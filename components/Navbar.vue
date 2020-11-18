2a553846a22154e5c18cdc7b3c508ce59dacb30b
laithkawar <lkawar95@gmail.com>
2020-11-18 04:01:40 +0200

    team update

---
+++ components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <div v-if="name" id="logo">
+      {{ name }}
+    </div>
+    <div v-else id="logo">
+      <img v-bind:src="logoImg" alt="Logo" />
+    </div>
+    <ul class="nav-links">
+      <li v-for="list in navLinks" :key="list.key">
+        <a v-if="list.dropdown === false" :href="list.link">{{ list.name }}</a>
+        <div class="dropdown-link" v-else>
+          <a :href="list.link">
+            {{ list.name }}
+
+            <span>&#x2193;</span>
+          </a>
+          <ul class="dropdown-menu">
+            <li v-for="item in list.dropdownLinks" :key="item.key">
+              <a :href="item.link">{{ item.name }}</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+    <div v-on:click="openMobileNav()" id="burger">
+      <div class="line1"></div>
+      <div class="line2"></div>
+      <div class="line3"></div>
+    </div>
+  </nav>
+</template>
