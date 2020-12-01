abd9dd458e635e8fe012d173c91ed35459707cc0
Kay <kay@localhost.localdomain>
2020-12-01 14:24:37 +1000

    SVG features

--- src/components/Benefits.vue
+++ src/components/Benefits.vue
@@ -35,10 +35,11 @@
               <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
             </svg>
           </div> -->
-          <BaseImg
-            src="payment.svg"
-            class="inline-flex items-center justify-center flex-shrink-0 w-20 h-20 mb-5 text-indigo-500 rounded-full"
-          />
+          <div
+            class="inline-flex items-center justify-center flex-shrink-0 w-20 h-20 mb-5 text-indigo-500 bg-indigo-100 rounded-full"
+          >
+            <BaseImg src="payment.svg" class="w-12" />
+          </div>
           <div class="flex-grow">
             <h2 class="mb-3 text-lg font-medium text-gray-900 title-font">
               ادفع فقط إذا كنت سعيدًا بتصميم الشعار
