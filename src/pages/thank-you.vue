c8678e251797d0dc44203155689c27ed96a83f3c
laithkawar <lkawar95@gmail.com>
2021-02-16 04:24:29 +0200

    messenger chatbot linked

--- src/pages/thank-you.vue
+++ src/pages/thank-you.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="py-12 bg-white mb-28">
     <div
-      class="max-w-2xl px-8 py-16 mx-auto text-center sm:py-20 sm:px-6 lg:px-8"
+      class="max-w-2xl px-8 py-6 mx-auto text-center sm:py-16 sm:px-6 lg:px-8"
     >
       <h2 class="text-3xl font-extrabold text-gray-600 sm:text-4xl">
         <span class="block">للأسف هذه الخدمة ليست جاهزة حاليا.</span>
@@ -13,13 +13,13 @@
       </h2>
       <p class="my-8 text-lg leading-6 text-gray-500">
         نحتاج مساعدتك لتحسين مربع! إذا كنت مهتم, سنسألك كم سؤال عن إحتياجاتك و
-        مشاكلك التصميميه على زوم.
+        مشاكلك التصميميه بمكالمة على فيسبوك مسنجر.
       </p>
       <a
-        href="calendly.com/morabba3/interviews"
-        class="inline-flex items-center justify-center w-full px-10 py-6 mt-10 text-base font-bold text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-50 sm:w-auto"
+        href="https://m.me/morabba3logos/"
+        class="inline-flex items-center justify-center w-full px-10 py-6 mt-6 text-base font-bold text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-300 sm:w-auto"
       >
-        إحجز دردشة زوم</a
+        إحجز دردشة الآن!</a
       >
     </div>
   </div>
